ad9b35ea5d74760ab4978b31893a9b55670dcda8
Aaron <aatwater@uwaterloo.ca>
2014-07-18 11:58:20 -0400

    updated go-chrome to halt on errors

--- go-chrome.sh
+++ go-chrome.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 npm install && bower install && grunt
+
+if [ $? -ne 0 ]; then
+  echo "Error npm/bower/grunt, terminating."
+  exit
+fi
+
 grunt dist-cr
+
+if [ $? -ne 0 ]; then
+  echo "Error building dist-cr, terminating."
+  exit
+fi
+
 cd dist
 rm -rf chrome
 unzip mailvelope.chrome.zip
